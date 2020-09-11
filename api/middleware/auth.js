417eb279637599fb4cec3af4bc1c4eb0cc7f4ddc
= <=>
2020-09-11 16:04:28 +0530

    update business users related table......update business user login / register functionality........add business user dashboard

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -6,7 +6,6 @@ module.exports = (req, res, next) => {
     try {
         //var token = req.body.token.split(' ')[1];
         var token = req.headers.authorization.split(' ')[1];
-        console.log(token);
         var decode = jwt.verify(token, 'secret');
         req.userdata = decode;
         next();
