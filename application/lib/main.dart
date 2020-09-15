0b927f98913aa8b04707365db425547368fece3e
rohit <817850204me@gmail.com>
2020-09-15 19:21:59 +0530

    abc

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:application/ui/tour/Tour_A.dart';
 import 'package:flutter/material.dart';
+import 'package:bot_toast/bot_toast.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Favorito',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+      debugShowCheckedModeBanner: false,
+      title: 'Favorito',
+      builder: BotToastInit(),
+      navigatorObservers: [BotToastNavigatorObserver()],
+      theme: ThemeData(
+        iconTheme: IconThemeData(
+          color: Colors.black, //change your color here
         ),
-        home: Tour_a(),
-      );
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Tour_a());
 }
--- application/lib/network/webservices.dart
+++ application/lib/network/webservices.dart
@@ -1,5 +1,6 @@
 import 'package:application/model/CatListModel.dart';
 import 'package:application/model/busyListModel.dart';
+import 'package:application/model/registerModel.dart';
 import 'package:application/network/serviceFunction.dart';
 import 'package:dio/dio.dart';
 import 'dart:convert' as convert;
@@ -19,6 +20,14 @@ class WebService {
     CatListModel _data = CatListModel();
     response = await dio.post(serviceFunction.funCatList, data: null);
     _data = CatListModel.fromJson(convert.json.decode(response.toString()));
+    print("responseData3:${_data.status}");
+    return _data;
+  }
+
+  static Future<registerModel> funRegister(Map _map) async {
+    registerModel _data = registerModel();
+    response = await dio.post(serviceFunction.funBusyRegister, data: _map);
+    _data = registerModel.fromJson(convert.json.decode(response.toString()));
     print("responseData3:${_data.status}");
     return _data;
   }
