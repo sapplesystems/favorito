f994acc9cdb6987ec2562b2c19aaa1a2de578cfb
rohit <8178520204me@gmail.com>
2020-09-15 12:41:19 +0530

    abc

--- application/lib/ui/setting/businessSetting.dart
+++ application/lib/ui/setting/businessSetting.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
+import 'package:application/component/roundedButton.dart';
+import 'package:application/component/txtfieldboundry.dart';
 import 'package:application/myCss.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -46,18 +50,123 @@ class _BusinessSettingState extends State<BusinessSetting> {
           width: context.percentWidth * 98,
           child: Stack(children: [
             Positioned(
-                top: 10,
-                left: 10,
+                top: context.percentHeight * 10,
+                left: context.percentWidth * 8,
+                right: context.percentWidth * 8,
                 child: Container(
                     decoration: bd1,
-                    child: ListTile(
-                      title: Text(
-                        "Your Business ID:",
-                        style: TextStyle(
-                          color: Colors.black,
+                    height: context.percentHeight * 70,
+                    padding: EdgeInsets.symmetric(
+                        horizontal: context.percentWidth * 6,
+                        vertical: context.percentHeight * 4),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding:
+                              EdgeInsets.only(top: context.percentHeight * 4),
+                          child: Image.asset(
+                            'assets/icon/foodcircle.png',
+                            fit: BoxFit.cover,
+                            height: context.percentHeight * 20,
+                          ),
                         ),
-                      ),
-                    )))
+                        Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: txtfieldboundry(
+                              title: "Business Name",
+                              security: false,
+                              hint: "Enter business name",
+                            )),
+                        Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: txtfieldboundry(
+                              title: "Business Phone",
+                              security: false,
+                              hint: "Enter business phone",
+                            )),
+                        Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: txtfieldboundry(
+                              title: "LandLine",
+                              security: false,
+                              hint: "Enter Landline number",
+                            )),
+                        Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Row(children: [Text("Business Hours")]),
+                                SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Column(children: [
+                                          Text(" Mon - Fri ",
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w400)),
+                                          Text("11:30 - 23:00",
+                                              style: TextStyle(
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.w200)),
+                                        ])
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Column(children: [
+                                          Text(" Mon - Fri ",
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w400)),
+                                          Text("11:30 - 23:00",
+                                              style: TextStyle(
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.w200)),
+                                        ])
+                                      ],
+                                    ),
+                                    Text("Add",
+                                        style: TextStyle(color: Colors.red))
+                                  ],
+                                )
+                              ],
+                            )),
+                      ],
+                    ))),
+            Positioned(
+                top: context.percentHeight * 4,
+                left: context.percentWidth * 18,
+                right: context.percentWidth * 18,
+                child: Container(
+                    decoration: bd1,
+                    padding: EdgeInsets.symmetric(
+                        horizontal: context.percentWidth * 6,
+                        vertical: context.percentHeight * 4),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Your Business ID",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          "6K146HB3A45",
+                          style: TextStyle(fontSize: 24),
+                        )
+                      ],
+                    ))),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: roundedButton(
+                clicker: () {},
+                clr: Colors.red,
+                title: "Sublit",
+              ),
+            )
           ]),
         ));
   }
