d5d1609ee53957c2eff04f16aef733c6ba3aa470
= <=>
2020-09-16 11:46:28 +0530

    add business sub category route.....update business category on the basis of business type and business sub category on the basis of business category

---
+++ api/controller/business_sub_category.js
@@ -0,0 +1,17 @@
+var db = require('../config/db');
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
+/*SELECT ALL BUSINESS CATEGORY*/
+exports.all_business_sub_category = function (req, res, next) {
+    if (req.body.category_id == '' || req.body.category_id == 'undefined' || req.body.category_id == null) {
+        return res.status(500).json({ status: 'error', message: 'Business category id required' });
+    }
+    var sql = "SELECT id, `sub_category_name` FROM business_sub_categories WHERE business_category_id='" + req.body.category_id + "' and is_activated='1' and deleted_at IS NULL";
+    db.query(sql, function (err, result) {
+        if (err) {
+            return res.status(500).json({ status: 'error', message: 'Something went wrong.' });
+        }
+        return res.status(200).json({ status: 'success', message: 'success', data: result });
+    });
+};
