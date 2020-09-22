cf5760499aa10e2d7c117fac14e16681d2b09527
rohit <8178520204me@gmail.com>
2020-09-22 20:08:17 +0530

    changed

--- application/lib/ui/login/login.dart
+++ application/lib/ui/login/login.dart
@@ -30,10 +30,11 @@ class _LoginState extends State<Login> {
       appBar: AppBar(
         backgroundColor: Color(0xfffff4f4),
         elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
+        leading: null,
+        // IconButton(
+        //   icon: Icon(Icons.arrow_back, color: Colors.black),
+        //   onPressed: () => Navigator.of(context).pop(),
+        // ),
         iconTheme: IconThemeData(
           color: Colors.black, //change your color here
         ),
