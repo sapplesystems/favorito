d86e6b0d80f5b25e668dbe31104c04f539d3ccc5
gautam-anand <gautam.anand@sapple.co.in>
2020-10-16 11:43:06 +0530

    commititng user app signup and log in page

---
+++ favorito_user/lib/utils/MyColors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color myBackGround = Color(0xffF9FAFF);
+Color myButtonBackground = Color(0xffEEF0F5);
+Color myEditTextBackground = Colors.white30;
+Color myRed = Color(0xffdd2626);
+Color myGrey = Color(0xff9996a3);
+Color myRedLight = Color(0x56dd2525);
